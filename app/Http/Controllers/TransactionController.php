a935c5b5f60b5b035e86d4a484012f24f724583d
محمد <mohammadakbarr18@gmail.com>
2026-03-05 08:12:06 +0700

    Menambahkan fitur transaksi Ringkasan Transaksi Bulanan & Tahunan

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -23,6 +23,62 @@ class TransactionController extends Controller
     }
 
     /**
+     * Display transaction summary grouped by year and month.
+     */
+    public function summary()
+    {
+        $transactions = Auth::user()
+            ->transactions()
+            ->orderBy('date', 'desc')
+            ->get();
+
+        // Group by Year -> Month
+        $groupedTransactions = [];
+
+        foreach ($transactions as $transaction) {
+            $year  = $transaction->date->format('Y');
+            $month = $transaction->date->format('m'); // numeric month for sorting
+
+            if (!isset($groupedTransactions[$year])) {
+                $groupedTransactions[$year] = [
+                    'year'          => $year,
+                    'total_income'  => 0,
+                    'total_expense' => 0,
+                    'months'        => [],
+                ];
+            }
+
+            if (!isset($groupedTransactions[$year]['months'][$month])) {
+                $groupedTransactions[$year]['months'][$month] = [
+                    'month_name'    => $transaction->date->translatedFormat('F'),
+                    'month_number'  => $month,
+                    'year'          => $year,
+                    'total_income'  => 0,
+                    'total_expense' => 0,
+                ];
+            }
+
+            $amount = (float) $transaction->amount;
+
+            if ($transaction->type === 'income') {
+                $groupedTransactions[$year]['total_income']                  += $amount;
+                $groupedTransactions[$year]['months'][$month]['total_income'] += $amount;
+            } else {
+                $groupedTransactions[$year]['total_expense']                  += $amount;
+                $groupedTransactions[$year]['months'][$month]['total_expense'] += $amount;
+            }
+        }
+
+        // Sort years descending, months descending within each year
+        krsort($groupedTransactions);
+        foreach ($groupedTransactions as &$yearData) {
+            krsort($yearData['months']);
+        }
+
+        return view('transactions-summary', compact('groupedTransactions'));
+    }
+
+    /**
      * Store a new transaction.
      */
     public function store(Request $request)
